5bf65cf8bd7cf22c16c1eb32b5e1fea6d2787edf
Luthfy Chaerudy <luthfy.chaerudy218@gmail.com>
2022-04-21 03:07:50 +0700

    all set, ongoing xendit and nodemailer

--- routes/shoppingroutes.js
+++ routes/shoppingroutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const ShopController = require('../Controller/shopController')
 
 router.get('/', ShopController.getAll)
-router.get('/:id', ShopController.detailAlbum)
-router.post('/:id', ShopController.createCart)
 router.get('/mycart', ShopController.viewMyCart)
 router.patch('/mycart', ShopController.buyAll)
+router.get('/:id', ShopController.detailAlbum)
+router.post('/:id', ShopController.createCart)
 
 module.exports = router
